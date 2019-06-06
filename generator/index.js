b5ede9cc00c2c2e96a541818a706172b25580a53
cmprat <cmprat@minsait.com>
2019-06-06 11:46:02 +0200

    added pug

--- generator/index.js
+++ generator/index.js
@@ -2,9 +2,24 @@ module.exports = (api, options) => {
   if (options.addODS) {
     api.extendPackage({
       dependencies: {
-        '@onesait/onesait-ds': '^1.0.0'
+        '@onesait/onesait-ds': '^0.1.7'
       }
     }),
+    api.registerCommand('', {})
     api.render('./template')
-  }
+  },
+  api.extendPackage({
+    dependencies: {
+      'pug-loader': '^2.4.0'
+    }
+  }),
+  api.chainWebpack(config =>
+    config.module
+      .rule('pug')
+      .test(/\.pug$/)
+        .uses
+          .delete('pug-plain-loader')
+          .end()
+        .use('pug-loader')
+          .loader('pug-loader')
 }
