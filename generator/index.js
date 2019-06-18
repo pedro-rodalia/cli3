e9714add1187be357cd96f700be4b9872c428e6e
cmprat <cmprat@minsait.com>
2019-06-18 12:50:08 +0200

    new changes

--- generator/index.js
+++ generator/index.js
@@ -16,15 +16,7 @@ module.exports = (api, options) => {
   api.injectImports(api.entryFile, "import ODS from '@onesait/onesait-ds' // eslint-disable-line")
 
   api.onCreateComplete(() => {
-    const { EOL } = require('os')
-    const fs = require('fs')
-    const contentMain = fs.readFileSync(api.entryFile, { encoding: 'utf-8' })
-    const lines = contentMain.split(/\r?\n/g)
-
-    const renderIndex = lines.findIndex(line => line.match(/Vue.config/))
-    lines[renderIndex] += `${EOL}  Vue.use(ODS),`
-
-    fs.writeFileSync(api.entryFile, lines.join(EOL), { encoding: 'utf-8' })
+    api.injectImports(api.entryFile, 'Vue.use(ODS)')
   })
 
   api.render('./template')
