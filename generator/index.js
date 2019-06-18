9550ec690e2fe22825ff260171d6656dc727bf82
cmprat <cmprat@minsait.com>
2019-06-18 10:59:42 +0200

    new changes

--- generator/index.js
+++ generator/index.js
@@ -11,7 +11,19 @@ module.exports = (api, options) => {
       'sass-resources-loader': '^2.0.1'
     }
   })
-  api.injectImports(api.entryFile, "import ODS from '@onesait/onesait-ds' // eslint-disable-line \nVue.use(ODS)")
   // api.injectImports(api.entryFile, 'Vue.use(ODS)')
   api.render('./template')
+  api.injectImports(api.entryFile, "import ODS from '@onesait/onesait-ds' // eslint-disable-line")
+  
+  api.onCreateComplete(() => {
+    const { EOL } = require('os')
+    const fs = require('fs')
+    const contentMain = fs.readFileSync(api.entryFile, { encoding: 'utf-8' })
+    const lines = contentMain.split(/\r?\n/g)
+
+    const renderIndex = lines.findIndex(line => line.match(/Vue.config/))
+    lines[renderIndex] += `${EOL}  Vue.use(ODS),`
+
+    fs.writeFileSync(api.entryFile, lines.join(EOL), { encoding: 'utf-8' })
+  })
 }
